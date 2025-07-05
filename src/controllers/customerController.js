2cee8a7afc295767873c2807d45ce736bcab95cf
darshanpadia <darshan.padia@somaiya.edu>
2025-07-05 15:19:45 +0530

    refactor: move customer creation logic from controller to service

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,34 +1,12 @@
 const customerService = require('../services/customerService');
-const Customer = require('../models/customerModel');
 
 function createCustomer(req, res, next) {
     try {
-        const { id, name, email } = req.body;
-
-        // check if customer already exists
-        const existing = customerService.getCustomerById(id);
-        if (existing) {
-            return res.status(409).json({
-                error: true,
-                message: `Customer with ID '${id}' already exists`
-            });
-        }
-
-        // Check if customer email already exists
-        const existingByEmail = customerService.getCustomerByEmail(email);
-        if (existingByEmail) {
-            return res.status(409).json({
-                error: true,
-                message: `Customer with email '${email}' already exists.`
-            });
-        }
-
-        const newCustomer = new Customer(id, name, email);
-        customerService.addCustomer(newCustomer);
-
+        const customer = customerService.createCustomer(req.body);
+        
         return res.status(201).json({
             message: "Customer created successfully",
-            data: newCustomer
+            data: customer
         });
     } catch (err) {
         next(err); // send to errorHandler middleware
--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,19 +1,44 @@
 const customers = []
+const Customer = require('../models/customerModel')
 
 function getCustomerById(id) {
     return customers.find(c => c.id === id);
 }
 
 function getCustomerByEmail(email) {
-    return customers.find(c => c.email.toLowerCase() === email.toLowerCase());
+    console.log(email)
+    if (email) {
+        return customers.find(c => c.email.toLowerCase() === email.toLowerCase());
+    }
 }
 
 function addCustomer(customer) {
     customers.push(customer);
 }
 
+function createCustomer({id, name, email}) {
+    console.log(id, name, email)
+    const existingById = getCustomerById(id);
+    if (existingById) {
+        const error = new Error(`Customer with ID '${id}' already exists`);
+        error.status = 409;
+        throw error;
+    }
+    const existingByEmail = getCustomerByEmail(email);
+    if (existingByEmail) {
+        const error = new Error(`Customer with email '${email}' already exists`);
+        error.status = 409;
+        throw error;
+    }
+
+    const newCustomer = new Customer(id, name, email);
+    addCustomer(newCustomer);
+    return newCustomer
+}
+
 module.exports = {
     getCustomerById,
     getCustomerByEmail,
-    addCustomer
+    addCustomer,
+    createCustomer
 }
